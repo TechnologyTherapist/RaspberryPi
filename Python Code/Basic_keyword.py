40e1381a73dc8855aebb6585fbda3b0e93b937d1
AKASH YADAV <59865773+TechnologyTherapist@users.noreply.github.com>
2021-12-28 20:11:31 +0530

    Initial commit

---
+++ Python Code/Basic_keyword.py
@@ -0,0 +1,12 @@
+# print("Hello Akash ")
+# print('hi')
+# a="Akash"
+# print(f"hi {a}")
+# print("""hii guys i am akash yadav from it branch so
+# good morning brother's
+# """)
+# ####
+# print("hii \f guys")
+# print("In python unicode character code \u03B2")
+# print("Akash"+"_Yadav")
+#
